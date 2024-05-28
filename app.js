ce12bd947d2cbd5776ea4514f5d9e010df92b4de
itsadi-24 <158060025+itsadi-24@users.noreply.github.com>
2024-05-28 14:06:35 +0530

    completed

--- app.js
+++ app.js
@@ -23,26 +23,56 @@ app.get('/', (req, res) => {
 
 io.on('connection', (uniquesocket) => {
   console.log('new user connected');
-});
-// whenever someone is connected,socket is that unique user that is connected
-//we have to set up socket.io at frontend also so that socketio at frontend is connected with socketio at backend
-if (!players.white) {
-  //if there is no white player then the player who is connected is white player and we are sending him response from backend to his browser(frontend)
-  players.white = uniquesocket.id;
-  uniquesocket.emit('playerRole', 'w');
-} else if (!players.black) {
-  players.black = uniquesocket.id;
-  uniquesocket.emit('playerRole', 'b');
-} else {
-  uniquesocket.emit('spectatorRole');
-  // if people join in full room
-}
-socket.on('disconnect', () => {
-  if (uniquesocket.id === players.white) {
-    delete players.white;
-  } else if (uniquesocket.id === players.black) {
-    delete players.black;
+
+  // whenever someone is connected,socket is that unique user that is connected
+  //we have to set up socket.io at frontend also so that socketio at frontend is connected with socketio at backend
+  if (!players.white) {
+    //if there is no white player then the player who is connected is white player and we are sending him response from backend to his browser(frontend)
+    players.white = uniquesocket.id;
+    uniquesocket.emit('playerRole', 'w');
+  } else if (!players.black) {
+    players.black = uniquesocket.id;
+    uniquesocket.emit('playerRole', 'b');
+  } else {
+    uniquesocket.emit('spectatorRole');
+    // if people join in full room
   }
+  uniquesocket.on('disconnect', () => {
+    if (uniquesocket.id === players.white) {
+      delete players.white;
+    } else if (uniquesocket.id === players.black) {
+      delete players.black;
+    }
+  });
+
+  uniquesocket.on('move', (move) => {
+    //this move will come from frontend
+    try {
+      //it means if the turn belongs to a player and other player tries to play it we will return instantly that is not valid move
+      if (chess.turn() === 'w' && uniquesocket.id !== players.white) return;
+      if (chess.turn() === 'b' && uniquesocket.id !== players.black) return;
+
+      const result = chess.move(move);
+      if (result) {
+        //if the move is valid then we will emit move event to all the players
+        currentPlayer = chess.turn(move);
+        io.emit('move', move);
+        io.emit('boardState', chess.fen());
+        if (chess.in_checkmate()) {
+          io.emit('message', 'Checkmate!');
+          io.emit('gameover');
+        } else if (chess.in_check()) {
+          io.emit('message', 'Check!');
+        }
+      } else {
+        console.log('Inavlid move :', move);
+        uniquesocket.emit('Invalid move', move);
+      }
+    } catch (err) {
+      console.log('Error:', err);
+      uniquesocket.emit('Invalid move', move);
+    }
+  });
 });
 
 server.listen(3000, () => {
